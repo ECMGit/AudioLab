d212824877757e1086be488495b6c63c0d8c4c86
reidaruss <reidarussell@gmail.com>
2020-09-24 14:52:53 -0500

    Added slider change frequency to module 2.

--- AudioLabSwift/Module2ViewController.swift
+++ AudioLabSwift/Module2ViewController.swift
@@ -9,22 +9,62 @@
 import UIKit
 
 class Module2ViewController: UIViewController {
-
+    let audio = AudioModel(buffer_size: AUDIO_BUFFER_SIZE)
+    var slider_freq = Float(15000.00)
+    lazy var graph:MetalGraph? = {
+        return MetalGraph(mainView: self.view)
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // add in graphs for display
+        graph?.addGraph(withName: "fft",
+                        shouldNormalize: true,
+                        numPointsInGraph: AUDIO_BUFFER_SIZE/2)
+        
+        graph?.addGraph(withName: "time",
+            shouldNormalize: false,
+            numPointsInGraph: AUDIO_BUFFER_SIZE)
+        
+        // just start up the audio model here
+        audio.startMicrophoneProcessing(withFps: 10)
+        //audio.startProcesingAudioFileForPlayback()
+        audio.startProcessingSinewaveForPlayback(withFreq: slider_freq)
+        audio.play()
+        
+        // run the loop for updating the graph peridocially
+        Timer.scheduledTimer(timeInterval: 0.05, target: self,
+            selector: #selector(self.updateGraph),
+            userInfo: nil,
+            repeats: true)
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func slider1(_ sender: UISlider) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        slider_freq = sender.value
+        audio.startProcessingSinewaveForPlayback(withFreq: slider_freq)
+        audio.play()
     }
-    */
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        audio.audioPause()
+        print("Exit, audio pause.")
+    }
+    
+    @objc
+    func updateGraph(){
+        self.graph?.updateGraph(
+            data: self.audio.fftData,
+            forKey: "fft"
+        )
+        
+        self.graph?.updateGraph(
+            data: self.audio.timeData,
+            forKey: "time"
+        )
+        
+    }
 
 }
