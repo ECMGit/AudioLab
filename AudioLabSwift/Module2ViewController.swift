5f805ac968d30ff929e784d4f82381da520c35d5
reidaruss <reidarussell@gmail.com>
2020-09-24 14:41:21 -0500

    Added module 2 view controller.

---
+++ AudioLabSwift/Module2ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Module2ViewController.swift
+//  Lab2
+//
+//  Created by Reid Russell on 9/24/20.
+//  Copyright © 2020 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class Module2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
